d41fb977516030958f02aca7b5e50746000364ba
gayathri-germanynrw <gayathrikasi@gmail.com>
2021-05-04 16:15:35 +0200

    Assignment 4 , created separate class for 4 problem.  Variable declaration moved into methods.  created separate Stringbuilder for each method

--- Assignment/src/assignment4/ProviderAssign4.java
+++ Assignment/src/assignment4/ProviderAssign4.java
@@ -1,23 +1,21 @@
 package assignment4;
 
 public class ProviderAssign4 {
-    private String revString;
-    private String chkStr;
-	private String finalStr;	
-    private String comStr;
-    private StringBuilder sb=new StringBuilder();
-	public void reverseString(String str) {
-		
-		
+	
+  	  public String reverseString(String str) {
+    	StringBuilder sb=new StringBuilder();
+    	String revString;
 		revString=sb.append(str).reverse().toString();
 		System.out.println("Reversed String\n"+revString);
-		sb.delete(0, sb.length());
+		sb.setLength(0);
+		return revString;
 			
 	}
 	
 	public void chkPalindrome(String chkPalindrome) {
-		
-		chkStr=sb.append(chkPalindrome).reverse().toString();
+		StringBuilder sb1=new StringBuilder();
+		String chkStr;
+		chkStr=sb1.append(chkPalindrome).reverse().toString();
 		if(chkStr.equals(chkPalindrome))
 		{
 			System.out.println("Entered String is an Palindrome\n"+chkPalindrome);
@@ -26,53 +24,27 @@ public class ProviderAssign4 {
 		else {
 			System.out.println("Entered String is not an Palindrome\n"+chkPalindrome);
 		}
-			
-		sb.delete(0, sb.length());
+		sb1.setLength(0);
 	}
 	public void strOperations() {
+		StringBuilder sb2=new StringBuilder();
 		String aboutTiger = "   The tiger is listed as Endangered on the IUCN Red List.";
-		sb.append(aboutTiger).append("The tiger is among the most recognisable and popular of the world's charismatic megafauna. "
+		sb2.append(aboutTiger).append("The tiger is among the most recognisable and popular of the world's charismatic megafauna. "
 				+ "As of 2015, the global wild tiger population was estimated to number between 3,062.");
 	   
 		String delString="IUCN";
-		int index=sb.indexOf(delString);
+		int index=sb2.indexOf(delString);
 		String repString="tiger"; 
-		int indRepString=sb.indexOf(repString);
-		sb.delete(index, index+delString.length());
-		sb.append( "The generic name Panthera is derived from the Latin word panthera");
-		sb.replace(indRepString, indRepString+repString.length(), "lion");
-		String endString=sb.toString();
+		int indRepString=sb2.indexOf(repString);
+		sb2.delete(index, index+delString.length());
+		sb2.append( "The generic name Panthera is derived from the Latin word panthera");
+		sb2.replace(indRepString, indRepString+repString.length(), "lion");
+		String endString=sb2.toString();
 	    System.out.println("print the result sentences\n"+endString.trim().toLowerCase());
-	    sb.delete(0, sb.length());
+	    sb2.setLength(0);
 	}
 	
 	
-	public  int findIndex(String[] animals,String str) {
-			int index=-1;
-			  for (int i=0;i<animals.length;i++) 
-			  {
-				  if(animals[i].equals(str)) index=i;
-					 
-			  }  
-				 
-			   return index;
-		
-		}
-		
-		public String processAnimal(String[] animals) {
-			
-	        animals[2]="Bear";
-	        
-			for (String string : animals) {
-			int index=findIndex(animals,string);
-			if(index > -1)
-			{
-				finalStr=sb.append(index).append(" ").append(string).append(" \n ").toString();
-			}
-		   }
-			return finalStr;
-			
-		}
 		
 		public void arrayOperations() {
 			
--- Assignment/src/assignment4/WildAnimals.java
+++ Assignment/src/assignment4/WildAnimals.java
@@ -2,13 +2,45 @@ package assignment4;
 
 public class WildAnimals {
 	
+
+	public  int findIndex(String[] animals,String str) {
+			int index=-1;
+			  for (int i=0;i<animals.length;i++) 
+			  {
+				  if(animals[i].equals(str)) index=i;
+					 
+			  }  
+				 
+			   return index;
+		
+		}
+		
+		public String processAnimal(String[] animals) {
+			StringBuilder sb3=new StringBuilder();
+	        animals[2]="Bear";
+	        String finalStr="";
+	        
+			for (String string : animals) {
+			int index=findIndex(animals,string);
+			if(index > -1)
+			{
+				finalStr=sb3.append(index).append(" ").append(string).append(" \n ").toString();
+			}
+		   }
+			return finalStr;
+			
+		}
+	
+		
 public static void main(String[] args) {
 	
 	String[] animals = {"Caiman", "Caiman Lizard", "Cairn Terrier", "Camel", "Camel Spider", "Canaan Dog", "Canadian Eskimo Dog", "Capybara", "Caracal", "Carolina Dog", "Carp", "Cassowary", "Cat", "Caterpillar", "Catfish", "Cavador", "Cavalier King Charles Spaniel", "Cavapoo", "Centipede","Cesky Fousek", "Cesky Terrier", "Chameleon",  "Chamois", "Cheagle", "Cheetah", "Chesapeake Bay Retriever", "Chicken", "Chihuahua",  "Chimaera", "Chimpanzee", "Chinchilla",
 			"Chinese Crested Dog",  "Chinese Paddlefish", "Chinook", 
 			"Chinstrap Penguin", "Chipmunk", "Chipoo"};
-	ProviderAssign4 pv4=new ProviderAssign4();
-	String comStr=pv4.processAnimal(animals);
+	String comStr;
+	WildAnimals wa=new WildAnimals();
+	
+	comStr=wa.processAnimal(animals);
 	System.out.println("---- String array with Animals ---\n");
 	System.out.println(comStr);
   	}
