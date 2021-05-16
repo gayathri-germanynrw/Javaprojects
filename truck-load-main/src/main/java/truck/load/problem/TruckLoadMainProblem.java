9351ff33cdc5899373723d6978df450ff060326f
gayathri-germanynrw <gayathrikasi@gmail.com>
2021-05-16 17:58:08 +0200

    Truck Load Problem commited

---
+++ truck-load-main/src/main/java/truck/load/problem/TruckLoadMainProblem.java
@@ -0,0 +1,17 @@
+package truck.load.problem;
+
+public class TruckLoadMainProblem {
+	
+
+	public static void main(String[] args) {
+		
+	
+		GetInputFromUser inputFromUsr=new GetInputFromUser();
+		// Get the values from the User
+		UserInput ui=inputFromUsr.getUserInput();
+		TruckLoadAlgorithm truckAlgorithm=new TruckLoadAlgorithm();
+		int maxDiff=truckAlgorithm.truckAlgorithmMethod(ui);
+        System.out.println("print the value of maxDiff----> "+maxDiff);
+	}
+
+}
---
+++ truck-load-main/src/main/java/truck/load/problem/UserInput.java
@@ -0,0 +1,29 @@
+package truck.load.problem;
+
+public class UserInput {
+   
+	  private int noOfTrucks;
+	  private int noOfBoxes;
+	  private int[] boxWeights;
+	public int getNoOfTrucks() {
+		return noOfTrucks;
+	}
+	public void setNoOfTrucks(int noOfTrucks) {
+		this.noOfTrucks = noOfTrucks;
+	}
+	public int getNoOfBoxes() {
+		return noOfBoxes;
+	}
+	public void setNoOfBoxes(int noOfBoxes) {
+		this.noOfBoxes = noOfBoxes;
+	}
+	public int[] getBoxweights() {
+		return boxWeights;
+	}
+	public void setBoxweights(int[] boxweights) {
+		this.boxWeights = boxweights;
+	}
+	  
+	
+	  
+}
