11bc311835425802fe1ca9e5c606da87e9ebadd3
gayathri-germanynrw <gayathrikasi@gmail.com>
2021-06-27 16:57:23 +0200

    comparator by Age,Name and Ranking

--- GkComparable/FootBallTeam.java
+++ GkComparable/FootBallTeam.java
@@ -9,14 +9,13 @@ import com.google.common.collect.ImmutableBiMap.Builder;
 
 public class FootBallTeam {
 	
-	
-	
 
 	public static void main(String[] args) {
 		List<Player> players=new ArrayList<>();
+		
 		players.add(Player.builder().withAge(12).withName("Ronalo").withRanking("A").build());
 		players.add(Player.builder().withAge(1).withName("John").withRanking("B").build());
-		players.add(Player.builder().withAge(2).withName("Tim").withRanking("c").build());
+		players.add(Player.builder().withAge(2).withName("Tim").withRanking("C").build());
 		players.add(Player.builder().withAge(3).withName("Knoop").withRanking("D").build());
 		System.out.println("Before sort"+players);
 		Comparator<Player> comparebyAge=(a,b) ->a.getAge() - b.getAge();
@@ -26,8 +25,12 @@ public class FootBallTeam {
 		Comparator<Player> comparebyRanking=(a,b) -> a.getRanking().compareTo(b.getRanking());
 		
 		Collections.sort(players,comparebyAge);
+		System.out.println("After sort compare by Age "+players);
+		Collections.sort(players, comparebyName);
+		System.out.println("After sort compare by Name"+players);
+		Collections.sort(players, comparebyRanking);
 		
-		System.out.println("After sort"+players);
+		System.out.println("After sort compare by Ranking "+players);
 	}
 	
 	
