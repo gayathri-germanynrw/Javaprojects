92e49232059d870bdf5d54ce0fec04d271ef2431
gayathri-germanynrw <gayathrikasi@gmail.com>
2022-01-09 00:19:55 +0100

    Exercise fourth program commit

---
+++ GkKleinerTaschenrechner/src/de/gkasi/Main.java
@@ -0,0 +1,42 @@
+package de.gkasi;
+
+
+/*
+Aufgabe 1. Kleiner TaschenrechnerErstellt ein neues Projekt mit dem Namen „XxKleinerTaschenrechner“.
+        Beim Programmstart soll der Programmname ausgegeben werden. Nachdem der User die eine der vier
+        Grundrechenarten (Addition, Subtraktion, Multiplikation, Division) gewählt hat, soll passend zu
+        der Auswahl die passende Funktion add, subtract, multipliy, divide aufgerufen werden.
+        Jede dieser Funktionen liestdie eingegebene Doublewerte einund packt diese in ein eigenes Array
+        welches Ihrnutzt um das Ergebnis zuberechnen, welches zurückgeliefert wird.
+*/
+
+
+
+public class Main {
+    private static final String APPLICATION_HEADER="############## Kleiner Taschen Rechner#######\n";
+
+
+    private static void printApplicationHeader(){
+        System.out.println(APPLICATION_HEADER);
+    }
+
+    public void  AdditionMethod()
+    {
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        printApplicationHeader();
+        System.out.println("Choose your Option \n");
+        int iIndexOfChoose=consoleR
+
+
+     void AdditionMethod();
+
+    }
+}
